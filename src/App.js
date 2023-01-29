5afbfe60f74a02c03b933db8b9f86c3a5666f8e7
Vishalvariya999 <110755112+Vishalvariya999@users.noreply.github.com>
2023-01-29 11:11:10 +0530

    Implement login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Form } from 'react-bootstrap'
+import "./App.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container mt-5'>
+        <div className='row'>
+          <div className='col-lg-6'>
+            <img src="../images/456.jpg" className='d-block w-100 ' alt="" />
+          </div>
+          <div className='col-lg-6 bg-white'>
+            <div className='my-auto'>
+              <div>
+                <h2 className='h2 text-center form-heading'>Welcome To Dreams</h2>
+              </div>
+              <form className='' action="">
+                <Form.Group className='mb-4 w-75 mx-auto '>
+                  <Form.Label>E-mail </Form.Label>
+                  <input className='form-input' type='email' placeholder='Enter Email Address'></input>
+                </Form.Group>
+                <Form.Group className='w-75 mx-auto'>
+                  <Form.Label>Paassword </Form.Label>
+                  <input className='form-input' type='password' placeholder='Enter Password'></input>
+                </Form.Group>
+                <Form.Group className='forgot-password'>
+                  <a href="#" className='form-forget'>Forget Password ?</a>
+                </Form.Group>
+                <Form.Group className='btnLogin d-flex justify-content-center'>
+                  <button type='button' className="btn btn-md btn-secondary w-25 rounded">Login</button>
+                </Form.Group>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
